d436bfcf1fb75b79b6fc8949dcf84010db539df2
Rajeev C P <rajeev.paul@axelerant.com>
2023-06-05 18:52:58 +0530

    FDR-293 CSL file chose UI creation and text changes

--- src/Form/IslandoraCitationFileForm.php
+++ src/Form/IslandoraCitationFileForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\islandora_citations\Form;
 
 use Drupal\Core\Form\FormStateInterface;
+use \DOMDocument;
 
 /**
  * IslandoraCitationFileForm form.
@@ -32,15 +33,58 @@ class IslandoraCitationFileForm extends IslandoraCitationForm {
   /**
    * {@inheritdoc}
    */
-  public function save(array $form, FormStateInterface $form_state) {
-    $result = parent::save($form, $form_state);
-    $message_args = ['%label' => $this->entity->label()];
-    $message = $result == SAVED_NEW
-      ? $this->t('Created new Islandora citation %label.', $message_args)
-      : $this->t('Updated Islandora citation %label.', $message_args);
-    $this->messenger()->addStatus($message);
-    $form_state->setRedirectUrl($this->entity->toUrl('collection'));
-    return $result;
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($file = $this->extractFile()) {
+      $content = file_get_contents($file->getRealPath());
+      
+      if ($this->isXMLContentValid($content)) {
+        $csl = simplexml_load_string($content);
+        $form_state->setValue('csl', $content);
+        parent::validateForm($form, $form_state);
+      } else {
+        $form_state->setErrorByName('file', $this->t('The uploaded file does not contain valid CSL.'));
+      }
+    }
+    else {
+      $form_state->setErrorByName('file', $this->t('The file could not be uploaded.'));
+    }
+  }
+
+/**
+ * @param string $xmlContent A well-formed XML string
+ * @param string $version 1.0
+ * @param string $encoding utf-8
+ * @return bool
+ */
+  function isXMLContentValid($xmlContent, $version = '1.0', $encoding = 'utf-8'){
+    if (trim($xmlContent) == '') {
+        return false;
+    }
+    libxml_use_internal_errors(true);
+    $doc = new DOMDocument($version, $encoding);
+    $doc->loadXML($xmlContent);
+    $errors = libxml_get_errors();
+    libxml_clear_errors();
+    return empty($errors);
+  }
+
+  /**
+   * Extract valid file from request.
+   *
+   * @return null|\Symfony\Component\HttpFoundation\File\UploadedFile
+   *   Uploaded file or NULL if file not uploaded.
+   */
+  protected function extractFile() {
+    $all_files = $this->getRequest()->files->get('files', []);
+    if (!empty($all_files['file'])) {
+      /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
+      $file = $all_files['file'];
+      if ($file->isValid()) {
+        return $file;
+      }
+    }
+
+    return NULL;
   }
 
 }
--- src/IslandoraCitationListBuilder.php
+++ src/IslandoraCitationListBuilder.php
@@ -16,7 +16,6 @@ class IslandoraCitationListBuilder extends ConfigEntityListBuilder {
   public function buildHeader() {
     $header['label'] = $this->t('Label');
     $header['id'] = $this->t('Machine name');
-    $header['status'] = $this->t('Status');
     return $header + parent::buildHeader();
   }
 
@@ -27,7 +26,6 @@ class IslandoraCitationListBuilder extends ConfigEntityListBuilder {
     /** @var \Drupal\islandora_citations\IslandoraCitationInterface $entity */
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
-    $row['status'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
     return $row + parent::buildRow($entity);
   }
 
