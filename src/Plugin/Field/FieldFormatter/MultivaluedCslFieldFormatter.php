c9b766e340613d1cbc5d8c9f752b3661f834d48b
Rajeev C P <rajeev.paul@axelerant.com>
2023-06-20 13:10:55 +0530

    FDR-321 FieldFormatter implementation for Multivalued fields

---
+++ src/Plugin/Field/FieldFormatter/MultivaluedCslFieldFormatter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\islandora_citations\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Plugin implementation of the 'Multivalued CSL Field' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "islandora_citations_multivalued_csl_field",
+ *   label = @Translation("Multivalued CSL Field"),
+ *   field_types = {
+ *     "string",
+ *     "datetime",
+ *   }
+ * )
+ */
+class MultivaluedCslFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+    $cslFields = $items->getFieldDefinition()->getThirdPartySetting('islandora_citations', 'csl_field');
+    if (empty($cslFields) || empty($items->value)) {
+      return [];
+    }
+
+    foreach ($items as $item) {
+      $data[] = $item->value;
+    }
+
+    $element = [
+      '#markup' => array_values($cslFields)[0] . ' => "' . implode(',', $data).'"',
+    ];
+
+    return $element;
+  }
+
+}
