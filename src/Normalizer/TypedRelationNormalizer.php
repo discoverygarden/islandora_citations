4e7caa22ef856b4a633b1e4063282dca671484d1
Akanksha Singh <akanksha@Akankshas-MacBook-Pro-2.local>
2023-06-27 18:33:26 +0530

    FDR-325: reformatted code and added a relationship check

--- src/Normalizer/TypedRelationNormalizer.php
+++ src/Normalizer/TypedRelationNormalizer.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\islandora_citations\Normalizer;
 
+use Drupal\taxonomy\TermInterface;
+
 /**
  * Converts Typed relation fields to an array including computed values.
  */
 class TypedRelationNormalizer extends NormalizerBase {
-
+  const PERSON_VOCAB = 'person';
 
   /**
    * The interface or class that this Normalizer supports.
@@ -19,27 +21,27 @@ class TypedRelationNormalizer extends NormalizerBase {
    * {@inheritdoc}
    */
   public function normalize($object, $format = NULL, array $context = []) {
-
     $attributes = [];
 
-    $bundle = $object->getParent()->entity->bundle();
-    if ($bundle == 'corporate_body') {
+    $parent = $object->getParent()->entity;
+    if ($parent instanceof TermInterface) {
       $rel_types = $object->getRelTypes();
-      $rel_type = $rel_types[$object->rel_type];
-      $attributes[$rel_type][] = $this->getNames($object->getParent()->entity->getName());
+      $rel_type = $this->formatRelTypes($rel_types[$object->rel_type]);
+      $label = $parent->getName();
+      if ($parent->bundle() === self::PERSON_VOCAB) {
+        $label = $this->formatNameVariables($label);
+      }
+
+      $attributes[$rel_type] = $label;
     }
-    elseif ($bundle == 'person') {
-      $rel_types = $object->getRelTypes();
-      $rel_type = $rel_types[$object->rel_type];
-      $attributes[$rel_type][] = $object->getParent()->entity->getName();
-    }
+
     return $attributes;
   }
 
   /**
-   * {@inheritdoc}
+   * Formats name variables as per csl json.
    */
-  public function getNames($name) {
+  private function formatNameVariables($name) {
     if (strpos($name, ',') !== FALSE) {
       $names = explode(',', $name);
       return is_array($names) ? $names[1] . ' || ' . $names[0] : $name;
@@ -48,6 +50,21 @@ class TypedRelationNormalizer extends NormalizerBase {
       $names = explode(' ', $name);
       return is_array($names) ? $names[0] . ' || ' . $names[1] : $name;
     }
+  }
+
+  /**
+   * Creates rel type mappings.
+   *
+   * Works for now but should be moved to third party settings.
+   */
+  private function formatRelTypes($rel_type) {
+    $rel_type = strtolower(trim(preg_replace("/\([^)]+\)/", "", $rel_type)));
+
+    if ($rel_type == 'creator') {
+      return 'author';
+    }
+
+    return $rel_type;
   }
 
 }
