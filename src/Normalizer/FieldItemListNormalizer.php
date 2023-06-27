e5b394abeed4807ace9064e795642e1363cbfa6f
Rajeev C P <rajeev.paul@axelerant.com>
2023-06-27 13:44:17 +0530

    FDR-321 multi-field normalizer

--- src/Normalizer/FieldItemListNormalizer.php
+++ src/Normalizer/FieldItemListNormalizer.php
@@ -32,21 +32,25 @@ class FieldItemListNormalizer extends NormalizerBase {
     // called 'value'). If that is the case, we will just return the content,
     // and not bother with the property name.
     $field_item_values = [];
+    $context['cardinality'] = $cardinality;
     foreach ($field_item_list as $field_item) {
       /** @var \Drupal\Core\Field\FieldItemInterface $field_item */
+
       $field_item_values[] = $this->serializer->normalize($field_item, $format, $context);
     }
-
     // If this is a single field, just consider the first item.
     if ($cardinality == 1) {
       $normalized_field['content'] = ['value' => reset($field_item_values)];
     }
     else {
+      foreach ($context['csl-map'] as $cslField) {
+        $content[$cslField] = implode(',', $field_item_values);
+      }
+      $normalized_field['content'] = ['value' => $content];
       // Comma separated values.
       // LinkedRelation - array values.
       // Paragraph - comma separated same field values.
     }
-
     return $normalized_field;
   }
 
