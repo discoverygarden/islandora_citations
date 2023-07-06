e6c90fb383fbf31d6e689107526ff160d395eccd
Rajeev C P <rajeev.paul@axelerant.com>
2023-07-06 16:17:21 +0530

    FDR-301 display citation

---
+++ src/Plugin/Block/DisplayCitationsBlock.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\islandora_citations\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a display citations block.
+ *
+ * @Block(
+ *   id = "islandora_citations_display_citations",
+ *   admin_label = @Translation("Display Citations"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class DisplayCitationsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Construct a new DisplayCitationsBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   The form builder instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['form'] = $this->formBuilder->getForm('Drupal\islandora_citations\Form\SelectCslForm');
+    return $build;
+  }
+
+}
