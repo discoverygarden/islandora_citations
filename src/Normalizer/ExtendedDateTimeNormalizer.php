b30e08e121ea713f95ff479ab8e9e39668faf411
Jordan Dukart <jordan@discoverygarden.ca>
2023-10-13 12:16:55 -0300

    Use a library instead.

--- src/Normalizer/ExtendedDateTimeNormalizer.php
+++ src/Normalizer/ExtendedDateTimeNormalizer.php
@@ -3,6 +3,8 @@
 namespace Drupal\islandora_citations\Normalizer;
 
 use Drupal\controlled_access_terms\Plugin\Field\FieldType\ExtendedDateTimeFormat;
+use EDTF\EdtfFactory;
+use EDTF\Model\ExtDate;
 
 /**
  * Converts EDTF fields to an array including computed values.
@@ -22,15 +24,16 @@ class ExtendedDateTimeNormalizer extends NormalizerBase {
   public function normalize($object, $format = NULL, array $context = []) {
     $dateValue = $object->getValue();
     if (!empty($dateValue['value'])) {
-      // Check if date is a range. We only support date at this point.
-      // @todo support other formats such as /date, date/, date ~ etc.
-      if (!preg_match('/[^a-zA-Z0-9-]+/', $dateValue['value'], $matches)) {
-        // Only contains dashes.
-        $dateParts = explode('-', $dateValue['value']);
+      $parser = EdtfFactory::newParser();
+      $parsed = $parser->parse($dateValue['value']);
+      $edtf = $parsed->getEdtfValue();
+      // XXX: Only support singular EDTF dates at this time, exit out if it's
+      // an interval or set.
+      if ($edtf instanceof ExtDate) {
+        return $this->formatDateVariables(explode('-', $edtf->iso8601()));
       }
     }
-
-    return $this->formatDateVariables($dateParts ?? $dateValue);
+    return [];
   }
 
 }
