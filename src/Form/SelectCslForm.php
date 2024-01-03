93613dffc559ac7b96e30166ef069d45e442a022
Prashant-bd <149510496+Prashant-bd@users.noreply.github.com>
2024-01-04 00:18:28 +0530

    Update src/Form/SelectCslForm.php
    Co-authored-by: Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>

--- src/Form/SelectCslForm.php
+++ src/Form/SelectCslForm.php
@@ -37,7 +37,7 @@ class SelectCslForm extends FormBase {
    *
    * @var string
    */
-  private $blockCSLAccessedDateFormate;
+  private $blockCSLAccessedDateFormat;
 
   /**
    * The route match.
